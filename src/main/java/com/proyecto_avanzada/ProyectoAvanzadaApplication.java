a76580d3b8e3d86fbad588a37412f3b0f6422952
Samuel Ortiz <samuortiz05@gmail.com>
2026-03-02 23:02:07 -0500

    estructura

---
+++ src/main/java/com/proyecto_avanzada/ProyectoAvanzadaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto_avanzada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAvanzadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAvanzadaApplication.class, args);
+	}
+
+}
